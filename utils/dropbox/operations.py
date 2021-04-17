2d3fa3299615dc01339f50b3aeb93441cb6e401a
Alex28499 <a.terrona@hotmail.es>
2021-04-17 12:29:29 +0200

    Txt y epub legibles

--- utils/dropbox/operations.py
+++ utils/dropbox/operations.py
@@ -1,8 +1,11 @@
 import dropbox
+import textract
 import os.path
 
 DROPBOX_OAUTH2_TOKEN = 'Me56EUxeX0MAAAAAAAAAAXkCEw6O5oYINF1YCi5PoGZm9xjFhQxFswcp2o_Kla8L'
 file_location = "/brainbook/"
+split_file=["titulo","tipo_fich"]
+
 
 dbx = dropbox.Dropbox(DROPBOX_OAUTH2_TOKEN)
 def upload_file (file):
@@ -10,11 +13,18 @@ def upload_file (file):
         dbx.files_upload(f.read(),file_location+file , mode=dropbox.files.WriteMode.overwrite)
 
 def read_file (file):
-    if os.path.isfile(file):
+    split_file = file.split(".",1)
+    txt_file = split_file[0]+"local.txt"
+    if os.path.isfile(txt_file):
         print("File exist")
     else:
         print("File not exist")
-        _, f =dbx.files_download(file_location+file)
-        f=f.content
-        f=f.decode("utf-8")
-        print(f)
+        dropbox_file=open(file,"x")
+        open (txt_file,'wb')
+        local_file = open(txt_file, "wb")
+        dbx.files_download_to_file(file,file_location+file)
+        text=textract.process(file, method='pdfminer') 
+        local_file.write(text)
+        local_file.close()
+        dropbox_file.close()
+        os.remove(file)  
--- utils/dropbox/test.py
+++ utils/dropbox/test.py
@@ -1,4 +1,4 @@
 from operations import*
-upload_file("sample.pdf")
-upload_file("Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub")
-read_file("sample1.txt")
+#read_file("sample1.txt")
+#read_file("Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub")
+read_file("sample.pdf")
